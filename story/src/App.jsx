8cca010ebad845112e49a99efb78223de0a775df
Adir <slutzkis@gmail.com>
2025-04-08 12:01:00 +0300

    updates

--- story/src/App.jsx
+++ story/src/App.jsx
@@ -7,13 +7,22 @@ import './App.css'
 function App() {
 
   return (
-    <div className='flex flex-col gap-4'>
-      <Tag label="Small Outline" shape="outline" size="small" />
-      <Tag label="Small Solid" shape="solid" size="small" />
-      
-      <Tag label="Medium Outline" shape="outline" size="medium" />
-      <Tag label="Medium Solid" shape="solid" size="medium" />
+    <div className='flex gap-4'>
+      <div className='flex flex-col gap-4'>
+        <Tag label="Small Outline" shape="outline" size="small" />
+        <Tag label="Small Solid" shape="solid" size="small" />
+
+        <Tag label="Medium Outline" shape="outline" size="medium" />
+        <Tag label="Medium Solid" shape="solid" size="medium" />
+      </div>
+      <div className='flex flex-col gap-4'>
+      <Tag label="Small Outline RO" shape="outline" size="small" ro="readonly" />
+      <Tag label="Small Solid RO" shape="solid" size="small" ro="readonly" />
+
+      <Tag label="Medium Outline RO" shape="outline" size="medium" ro="readonly" />
+      <Tag label="Medium Solid RO" shape="solid" size="medium" ro="readonly" />
     </div>
+    </div >
   )
 }
 
--- story/src/components/Tag.jsx
+++ story/src/components/Tag.jsx
@@ -3,8 +3,11 @@ import './Tag.css'
 
 export default function Tag(props) {
     return (
-        <div className={`flex text-xs bg-zinc-200 px-3 py-2 rounded-3xl size-fit ${props.size} ${props.shape}`}>
+        <div className={`flex gap-1 text-xs bg-zinc-200 px-3 py-2 rounded-3xl size-fit cursor-pointer font-[inter] ${props.size} ${props.shape} ${props.ro} hover:bg-zinc-300`}>
             {props.label}
+            <span class="material-symbols-rounded text-base! leading-none">
+                close
+            </span>
         </div>
     );
 }
