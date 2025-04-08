0727947c6386d6fdb68c26a19826160e63867267
Adir <slutzkis@gmail.com>
2025-04-08 12:04:28 +0300

    updates

--- story/src/components/Tag.jsx
+++ story/src/components/Tag.jsx
@@ -3,7 +3,7 @@ import './Tag.css'
 
 export default function Tag(props) {
     return (
-        <div className={`flex gap-1 text-xs bg-zinc-200 px-3 py-2 rounded-3xl size-fit cursor-pointer font-[inter] ${props.size} ${props.shape} ${props.ro} hover:bg-zinc-300`}>
+        <div className={`flex gap-1 text-xs bg-zinc-200 px-3 py-2 rounded-3xl size-fit cursor-pointer select-none font-[inter] ${props.size} ${props.shape} ${props.ro} hover:bg-zinc-300`}>
             {props.label}
             <span class="material-symbols-rounded text-base! leading-none text-black">
                 close
