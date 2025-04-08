06472d553d23b1e72a4bb53dd49ce80d55b9e2ec
Adir <slutzkis@gmail.com>
2025-04-08 17:46:03 +0300

    Update Tag.stories.js

--- story/src/stories/Tag.stories.js
+++ story/src/stories/Tag.stories.js
@@ -6,6 +6,7 @@ import Tag from "../components/Tag";
 export default {
   title: 'Components/Tag',
   component: Tag,
+  
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
