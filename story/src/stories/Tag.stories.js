0a0d1909f4cee39b4591d48d652a77ba8f112695
Adir <slutzkis@gmail.com>
2025-04-09 10:24:52 +0300

    Update Tag.stories.js

--- story/src/stories/Tag.stories.js
+++ story/src/stories/Tag.stories.js
@@ -6,14 +6,15 @@ import Tag from "../components/Tag";
 export default {
   title: 'Components/Tag',
   component: Tag,
-  
+
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
   argTypes: {
     label: { control: "text" },
-    icon: { control: "text" },
+    showIcon: { control: "boolean" }, // Toggle to enable/disable icon
+    icon: { control: "text", if: { arg: "showIcon", truthy: true } }, // Only show when `showIcon` is true
     size: { control: "radio", options: ["small", "medium"] },
     shape: { control: "radio", options: ["solid", "outline"] },
     ro: { control: "radio", options: ["readonly", "editable"] },
@@ -24,6 +25,7 @@ export default {
 export const Editable = {
   args: {
     label: 'Tag label',
+    showIcon: true,
     icon: 'schedule',
     size: 'medium',
     shape: 'solid',
@@ -34,6 +36,7 @@ export const Editable = {
 export const ReadOnly = {
   args: {
     label: 'Tag label',
+    showIcon: true,
     icon: 'schedule',
     size: 'medium',
     shape: 'solid',
