18e02a4db8befa77d22fd63c46a92a56c77b21d9
Adir <slutzkis@gmail.com>
2025-04-10 12:17:01 +0300

    updates

--- story/src/App.jsx
+++ story/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='flex gap-4'>
       <div className='flex flex-col gap-4'>
-        <Avatar label="BS" />
+        <Avatar label="BS" size="lg"/>
       </div>
       <div className='flex flex-col gap-4'>
         <Tag label="Small Outline" shape="outline" size="small" />
--- story/src/components/Avatar.jsx
+++ story/src/components/Avatar.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-import './Tag.css'
+import './Avatar.css'
 
 export default function Tag(props) {
     return (
-        <div className="avatar">
+        <div className={`avatar flex gap-1 bg-blue-500 rounded-full size-${props.size}`}>
             {props.label}
         </div>
     );
