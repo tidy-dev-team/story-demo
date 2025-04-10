91e7808df5de11a677a0552879b8087ded354180
Adir <slutzkis@gmail.com>
2025-04-10 12:01:24 +0300

    Avatar

--- story/src/App.jsx
+++ story/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='flex gap-4'>
       <div className='flex flex-col gap-4'>
-        <Avatar />
+        <Avatar label="BS" />
       </div>
       <div className='flex flex-col gap-4'>
         <Tag label="Small Outline" shape="outline" size="small" />
